01b627e2c8fb8059cd8ac69f7440861e7c9e783f
Densau <nerchio1@gmail.com>
2022-12-04 14:10:22 +0100

    day4

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,47 @@
+// DAY 4
+
+private var testFile: String = "day4_test.txt";
+private var inputFile: String = "day4_input.txt";
+
+private fun readFileLineByLineUsingForEachLine(givenFile: String): List<String> =
+    object {}.javaClass.getResourceAsStream(givenFile)!!.bufferedReader().readLines()
+
+
+private fun findDayFourSolutions(givenFile: String) {
+    val givenInput: List<String> = readFileLineByLineUsingForEachLine(givenFile)
+    findOneStarSolution(givenInput)
+    findTwoStarSolution(givenInput)
+}
+
+private fun assignmentFullOverlap(firstAssignment: List<Int>, secondAssignment: List<Int>): Boolean {
+    return (firstAssignment[0] <= secondAssignment[0] && firstAssignment[1] >= secondAssignment[1])
+}
+
+private fun assignmentPartialOverlap(firstAssignment: List<Int>, secondAssignment: List<Int>): Boolean {
+    return (firstAssignment[0] >= secondAssignment[0] && firstAssignment[0] <= secondAssignment[1]) ||
+            (firstAssignment[1] >= secondAssignment[0] && firstAssignment[1] <= secondAssignment[1])
+}
+
+private fun findTwoStarSolution(givenInput: List<String>) {
+    println(givenInput.count { line ->
+        var elfAssignments = line.split(",")
+        var firstElf = elfAssignments[0].split("-").map { e -> Integer.parseInt(e) }
+        var secondElf = elfAssignments[1].split("-").map { e -> Integer.parseInt(e) }
+        assignmentPartialOverlap(firstElf, secondElf) || assignmentPartialOverlap(secondElf, firstElf)
+    })
+}
+
+private fun findOneStarSolution(givenInput: List<String>) {
+    println(givenInput.count { line ->
+        var elfAssignments = line.split(",")
+        var firstElf = elfAssignments[0].split("-").map { e -> Integer.parseInt(e) }
+        var secondElf = elfAssignments[1].split("-").map { e -> Integer.parseInt(e) }
+        assignmentFullOverlap(firstElf, secondElf) || assignmentFullOverlap(secondElf, firstElf)
+    })
+}
+
+fun main() {
+    findDayFourSolutions(testFile)
+    findDayFourSolutions(inputFile)
+}
+
