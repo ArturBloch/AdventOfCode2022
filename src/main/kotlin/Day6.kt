12c9dddf47bbf496ca8ea21c226a308446a4c65c
Densau <nerchio1@gmail.com>
2022-12-06 18:58:57 +0100

    day6 was fast

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,39 @@
+// DAY 6
+
+private var testFile: String = "day6_test.txt";
+private var inputFile: String = "day6_input.txt";
+
+private fun readFileLineByLineUsingForEachLine(givenFile: String): List<String> =
+    object {}.javaClass.getResourceAsStream(givenFile)!!.bufferedReader().readLines()
+
+
+private fun findDaySixolutions(givenFile: String) {
+    val givenInput: List<String> = readFileLineByLineUsingForEachLine(givenFile)
+    findOneStarSolution(givenInput)
+    findTwoStarSolution(givenInput)
+}
+
+private fun findUniqueCharSequence(givenString: String, length: Int): Int{
+    var signalFound = false
+    var index = length
+    while(!signalFound){
+        var charSet = givenString.substring(index - length, index).toHashSet()
+        if(charSet.size == length) break
+        index++
+    }
+    return index
+}
+
+private fun findTwoStarSolution(givenInput: List<String>) {
+    println(findUniqueCharSequence(givenInput[0], 14))
+}
+
+private fun findOneStarSolution(givenInput: List<String>) {
+    println(findUniqueCharSequence(givenInput[0], 4))
+}
+
+fun main() {
+    findDaySixolutions(testFile)
+    findDaySixolutions(inputFile)
+}
+
